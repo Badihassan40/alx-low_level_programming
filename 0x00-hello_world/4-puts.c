4245559987c9627c92867bce5654991810bb1b18
hassan badi <hassanbadi40@gmail.com>
2023-06-15 17:48:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints "programming is like building a multilingual puzzle, followed by a neline."
+ * Return 0.
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+    return (0);
+}
